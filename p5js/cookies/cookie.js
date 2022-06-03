e9ca4343f5622f4f877eaba37068b60bbfd72a77
AurumDoinPrograming <106434031+AurumDoinPrograming@users.noreply.github.com>
2022-06-03 08:32:29 +0200

    Delete p5js directory

--- p5js/cookies/cookie.js
+++
@@ -1,8 +0,0 @@
-var mycookie = Cookies.get("mycookie");
-name = Cookies.set("mycookie", true);
-console.log("cookie:\n", mycookie);
-
-
-var annmerkning = Cookies.get("annmerkning");
-name = Cookies.set("annmerkning", false);
-console.log("cookie:\n", annmerkning);
